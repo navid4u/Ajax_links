b1934607a5d4e7f2070bef1e5d3e53f667821773
Navid Nasrollahi <106493156+navid4u@users.noreply.github.com>
2023-05-16 09:35:53 +0330

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("link").innerHTML = this.responseText;
+     document.getElementById("page").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "page_1.php", true);
@@ -12,6 +12,21 @@
 
     
     <html>
-                            <li  onclick="loadProfile()"><a> <i> <span>My link</span> </i></li>
+    <div id="link">                        
+        <li  onclick="loadProfile()"><a> <i> <span>My link</span> </i></li>
+    </div>
+    
+    
+     <div id="page">
+         <h1>
+         This is the body of the page
+         </h1>
+     </div>
+
+    
+    
+    
+    
+    
     
     </html>
